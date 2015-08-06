9bb91974c0eba6e488cbf82a7bae167478eaabd2
Long Nguyen <long.nguyen11288@gmail.com>
2015-08-06 12:02:03 -0400

    updated shipit script

--- ci/scripts/shipit.sh
+++ ci/scripts/shipit.sh
@@ -18,26 +18,8 @@ if [[ "${release_name}X" == "X" ]]; then
   exit 1
 fi
 
-cd ../
-mkdir -p $GOPATH/src/github/geofffranks/
-cp -r spruce $GOPATH/src/github/geofffranks/.
-pushd $GOPATH/src/github/geofffranks/spruce
-
-godep restore
-
-echo Prepare github release information
-set -x
-mkdir -p releases
-cp ci/release_notes.md releases/notes.md
-echo "${release_name} v${version}" > releases/name
-echo "v${version}" > releases/tag
-# Update version
-sed -i '' -e "s/var VERSION string = \".*\"/var VERSION string = \"${version}\"/" main.go
-
-
-goxc -bc="linux,!arm darwin,amd64" -d=releases -pv=${version}
-
-cp -r releases $DIR/../../.
+sed -i -e "s/var VERSION string = \".*\"/var VERSION string = \"${version}\"/" main.go
+sed -i -e "s/Version .*\\\n/Version ${version}\\\n/" main_test.go
 
 git config --global user.email "drnic+bot@starkandwayne.com"
 git config --global user.name "CI Bot"
@@ -46,3 +28,25 @@ git merge --no-edit ${promotion_branch}
 
 git add -A
 git commit -m "update release version to v${version}"
+
+
+echo Prepare github release information
+set -x
+mkdir -p releases
+cp ci/release_notes.md releases/notes.md
+echo "${release_name} v${version}" > releases/name
+echo "v${version}" > releases/tag
+# Update version
+
+cd ../
+mkdir -p $GOPATH/src/github/geofffranks/
+cp -r spruce $GOPATH/src/github/geofffranks/.
+pushd $GOPATH/src/github/geofffranks/spruce
+
+godep restore
+
+go build .
+./spruce -v 2>&1 | grep "./spruce - Version ${version}"
+
+goxc -bc="linux,!arm darwin,amd64" -d=$DIR/../../releases -pv=${version}
+
