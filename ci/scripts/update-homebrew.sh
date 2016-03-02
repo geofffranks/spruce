e74018750a14120bfb0f3dc5208351f5d26ef3bf
Geoff Franks <geoff.franks@gmail.com>
2016-03-01 21:26:45 -0500

    Allow no-op homebrew updates

--- ci/scripts/update-homebrew.sh
+++ ci/scripts/update-homebrew.sh
@@ -55,20 +55,22 @@ echo ">> Updating $formula with new version/shasum"
 auto_sed "s/v = \\\".*\\\" # CI Managed/v = \\\"v${VERSION}\\\" # CI Managed/" $formula
 auto_sed "s/sha256 \\\".*\\\" # CI Managed/sha256 \\\"${SHASUM}\\\" # CI Managed/" $formula
 
-set +e
-if [[ -z $(git config --global user.email) ]]; then
-  git config --global user.email "drnic+bot@starkandwayne.com"
+if [[ "$(git status -s)X" != "X" ]]; then
+  set +e
+  if [[ -z $(git config --global user.email) ]]; then
+    git config --global user.email "drnic+bot@starkandwayne.com"
+  fi
+  if [[ -z $(git config --global user.name) ]]; then
+    git config --global user.name "CI Bot"
+  fi
+  
+  set -e
+  echo ">> Running git operations as $(git config --global user.name) <$(git config --global user.email)>"
+  echo ">> Getting back to master (from detached-head)"
+  git merge --no-edit master
+  git --no-pager diff
+  git add $formula
+  git commit -m "Updated $formula from new release"
+else
+  echo ">> No update needed"
 fi
-if [[ -z $(git config --global user.name) ]]; then
-  git config --global user.name "CI Bot"
-fi
-
-set -e
-echo ">> Running git operations as $(git config --global user.name) <$(git config --global user.email)>"
-echo ">> Getting back to master (from detached-head)"
-git merge --no-edit master
-git --no-pager diff
-git add $formula
-git commit -m "Updated $formula from new release"
-
-exit 0
