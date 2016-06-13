b85d51e19cf8c89606d70ae226c91734cb542d71
Satish Puranam <satish.puranam@gmail.com>
2016-06-13 14:45:59 -0400

    -v and --version should send output to stdout

--- cmd/spruce/main_test.go
+++ cmd/spruce/main_test.go
@@ -175,8 +175,8 @@ func TestMain(t *testing.T) {
 				stdout = ""
 				stderr = ""
 				main()
-				So(stdout, ShouldEqual, "")
-				So(stderr, ShouldStartWith, fmt.Sprintf("spruce - Version %s", Version))
+				So(stdout, ShouldStartWith, fmt.Sprintf("spruce - Version %s", Version))
+				So(stderr, ShouldEqual, "")
 				So(rc, ShouldEqual, 0)
 			})
 		})
