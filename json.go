750d4ff6a94e610ec4c4738f027c37bec290f374
Soren Hansen <Soren.Hansen@mastercard.com>
2018-04-19 10:37:09 +0200

    Add strict JSONification mode
    Fixes #259

--- json.go
+++ json.go
@@ -12,7 +12,7 @@ import (
 	. "github.com/geofffranks/spruce/log"
 )
 
-func jsonifyData(data []byte) (string, error) {
+func jsonifyData(data []byte, strict bool) (string, error) {
 	y, err := simpleyaml.NewYaml(data)
 	if err != nil {
 		return "", err
@@ -23,7 +23,12 @@ func jsonifyData(data []byte) (string, error) {
 		return "", ansi.Errorf("@R{Root of YAML document is not a hash/map}: %s\n", err.Error())
 	}
 
-	b, err := json.Marshal(deinterface(doc))
+	doc_, err := deinterface(doc, strict)
+	if err != nil {
+		return "", err
+	}
+
+	b, err := json.Marshal(doc_)
 	if err != nil {
 		return "", err
 	}
@@ -31,15 +36,15 @@ func jsonifyData(data []byte) (string, error) {
 	return string(b), nil
 }
 
-func JSONifyIO(in io.Reader) (string, error) {
+func JSONifyIO(in io.Reader, strict bool) (string, error) {
 	data, err := ioutil.ReadAll(in)
 	if err != nil {
 		return "", ansi.Errorf("@R{Error reading input}: %s", err)
 	}
-	return jsonifyData(data)
+	return jsonifyData(data, strict)
 }
 
-func JSONifyFiles(paths []string) ([]string, error) {
+func JSONifyFiles(paths []string, strict bool) ([]string, error) {
 	l := make([]string, len(paths))
 
 	for i, path := range paths {
@@ -49,7 +54,7 @@ func JSONifyFiles(paths []string) ([]string, error) {
 			return nil, ansi.Errorf("@R{Error reading file} @m{%s}: %s", path, err)
 		}
 
-		if l[i], err = jsonifyData(data); err != nil {
+		if l[i], err = jsonifyData(data, strict); err != nil {
 			return nil, ansi.Errorf("%s: %s", path, err)
 		}
 	}
@@ -57,35 +62,60 @@ func JSONifyFiles(paths []string) ([]string, error) {
 	return l, nil
 }
 
-func deinterface(o interface{}) interface{} {
+func deinterface(o interface{}, strict bool) (interface{}, error) {
 	switch o.(type) {
 	case map[interface{}]interface{}:
-		return deinterfaceMap(o.(map[interface{}]interface{}))
+		return deinterfaceMap(o.(map[interface{}]interface{}), strict)
 	case []interface{}:
-		return deinterfaceList(o.([]interface{}))
+		return deinterfaceList(o.([]interface{}), strict)
 	default:
-		return o
+		return o, nil
 	}
 }
 
-func deinterfaceMap(o map[interface{}]interface{}) map[string]interface{} {
+func addKeyToMap(m map[string]interface{}, k interface{}, v interface{}, strict bool) (error) {
+	vs := fmt.Sprintf("%v", k)
+	_, exists := m[vs]
+	if exists {
+		NewWarningError(eContextAll, "@Y{Duplicate key detected: %s}", vs).Warn()
+		return nil
+	}
+	dv, err := deinterface(v, strict)
+	if err != nil {
+		return err
+	}
+	m[vs] = dv
+	return nil
+}
+
+func deinterfaceMap(o map[interface{}]interface{}, strict bool) (map[string]interface{}, error) {
 	m := map[string]interface{}{}
 	for k, v := range o {
-		vs := fmt.Sprintf("%v", k)
-		_, exists := m[vs]
-		if exists {
-			NewWarningError(eContextAll, "@Y{Duplicate key detected: %s}", vs).Warn()
-			continue
+		switch k.(type) {
+		case string:
+			err := addKeyToMap(m, k, v, strict)
+			if err != nil {
+				return nil, err
+			}
+		default:
+			if (strict) {
+		               return nil, fmt.Errorf("Non-string keys found during strict JSON conversion")
+			} else {
+				addKeyToMap(m, k, v, strict)
+			}
 		}
-		m[vs] = deinterface(v)
 	}
-	return m
+	return m, nil
 }
 
-func deinterfaceList(o []interface{}) []interface{} {
+func deinterfaceList(o []interface{}, strict bool) ([]interface{}, error) {
 	l := make([]interface{}, len(o))
 	for i, v := range o {
-		l[i] = deinterface(v)
+		v_, err := deinterface(v, strict)
+		if err != nil {
+			return nil, err
+		}
+		l[i] = v_
 	}
-	return l
+	return l, nil
 }
