a97d5874c3bf8bd299bca3dee632abefdbd1fab1
Geoff Franks <geoff.franks@gmail.com>
2026-04-17 18:26:53 -0400

    test: use go tool counterfeiter directive
    Match project convention (gosec, staticcheck) which invokes tool-block
    binaries via "go tool" rather than "go run".

--- fakes/generate.go
+++ fakes/generate.go
@@ -1,4 +1,4 @@
 package fakes
 
-//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o fake_ssm_api.go github.com/aws/aws-sdk-go/service/ssm/ssmiface.SSMAPI
-//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o fake_secrets_manager_api.go github.com/aws/aws-sdk-go/service/secretsmanager/secretsmanageriface.SecretsManagerAPI
+//go:generate go tool counterfeiter -o fake_ssm_api.go github.com/aws/aws-sdk-go/service/ssm/ssmiface.SSMAPI
+//go:generate go tool counterfeiter -o fake_secrets_manager_api.go github.com/aws/aws-sdk-go/service/secretsmanager/secretsmanageriface.SecretsManagerAPI
