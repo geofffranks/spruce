fb5932d4926bb6fb18adf31686b2a9aa9a0b7497
Geoff Franks <geoff.franks@gmail.com>
2026-04-17 19:03:06 -0400

    test: migrate diff tests to Ginkgo

--- diff_test.go
+++ diff_test.go
@@ -5,75 +5,73 @@ import (
 	"os"
 	"regexp"
 	"strings"
-	"testing"
 	"unicode"
 
-	// Use geofffranks forks to persist the fix in https://github.com/go-yaml/yaml/pull/133/commits
-	// Also https://github.com/go-yaml/yaml/pull/195
 	"github.com/geofffranks/simpleyaml"
-	. "github.com/smartystreets/goconvey/convey"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-func TestDiff(t *testing.T) {
+var _ = Describe("Diff", func() {
 	YAML := func(s string) map[interface{}]interface{} {
 		y, err := simpleyaml.NewYaml([]byte(s))
-		So(err, ShouldBeNil)
+		Expect(err).NotTo(HaveOccurred())
 
 		data, err := y.Map()
-		So(err, ShouldBeNil)
+		Expect(err).NotTo(HaveOccurred())
 
 		return data
 	}
 
 	trim := func(s string) string {
-		/* find the shortest prefix of space characters */
 		shortest := regexp.MustCompile(`^ +`).FindString(s)
 		s = strings.TrimRightFunc(s, unicode.IsSpace)
 		return regexp.MustCompile(`(?m)^`+shortest).ReplaceAllString(s, "")
 	}
 
-	var test, a, b, diff string
-	var current *string
-	runtest := func() {
-		if test == "" {
-			return
-		}
-		Convey(test, t, func() {
-			d, err := Diff(YAML(a), YAML(b))
-			So(err, ShouldBeNil)
-			So(trim(d.String("$")), ShouldEqual, trim(diff))
-		})
-	}
+	It("passes all diff test cases from tests/diff", func() {
+		var test, a, b, diff string
+		var current *string
+		testPat := regexp.MustCompile(`^###+\s+(.*)\s*$`)
 
-	testPat := regexp.MustCompile(`^###+\s+(.*)\s*$`)
-	in, err := os.Open("tests/diff")
-	if err != nil {
-		panic(err)
-	}
-
-	s := bufio.NewScanner(in)
-	for s.Scan() {
-		if testPat.MatchString(s.Text()) {
-			m := testPat.FindStringSubmatch(s.Text())
-			runtest()
-			test, a, b, diff = m[1], "", "", ""
-			continue
-		}
-
-		if s.Text() == "---" {
-			if a == "" {
-				current = &a
-			} else if b == "" {
-				current = &b
-			} else {
-				current = &diff
+		runtest := func() {
+			if test == "" {
+				return
 			}
-			continue
+			By(test)
+			d, err := Diff(YAML(a), YAML(b))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(trim(d.String("$"))).To(Equal(trim(diff)))
 		}
 
-		if current != nil {
-			*current = *current + s.Text() + "\n"
+		in, err := os.Open("tests/diff")
+		Expect(err).NotTo(HaveOccurred())
+		defer in.Close()
+
+		s := bufio.NewScanner(in)
+		for s.Scan() {
+			if testPat.MatchString(s.Text()) {
+				m := testPat.FindStringSubmatch(s.Text())
+				runtest()
+				test, a, b, diff = m[1], "", "", ""
+				continue
+			}
+
+			if s.Text() == "---" {
+				if a == "" {
+					current = &a
+				} else if b == "" {
+					current = &b
+				} else {
+					current = &diff
+				}
+				continue
+			}
+
+			if current != nil {
+				*current = *current + s.Text() + "\n"
+			}
 		}
-	}
-	runtest()
-}
+		runtest()
+	})
+})
